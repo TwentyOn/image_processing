1f9d0c24742cc9e0f1b1a9a4052c17a4e817fc67
Aleksey <aleksey-000@mail.ru>
2025-08-21 11:33:46 +0300

    Выполнено подключение к S3-хранилищу (тест). Конечная точка теперь возвращает ссылку на файлы из S3-хранилища

--- image_processing_api/views.py
+++ image_processing_api/views.py
@@ -3,6 +3,8 @@ import os
 from zipfile import ZipFile
 from datetime import datetime, date
 from time import perf_counter
+import subprocess
+import tempfile
 
 import cairosvg
 from django.shortcuts import render
@@ -20,6 +22,7 @@ from cairosvg import svg2png
 import io
 
 from .serializers import InputImage
+from .minio_bucket import bucket
 
 
 def index(request):
@@ -46,17 +49,29 @@ class ImageProcessing(APIView):
                 output_zip_filename = f'output_{datetime_name_mark}.zip'
                 path = os.path.join(settings.MEDIA_ROOT, f'download\\{output_zip_filename}')
                 if self.zip_processing(file, serializer.validated_data, path, datetime_name_mark):
-                    file_url = request.build_absolute_uri(
-                        os.path.join(settings.MEDIA_URL, f'download/{output_zip_filename}'))
-                    # print(perf_counter() - start_time)
-                    return Response({'status_code': status.HTTP_200_OK, 'file_url': file_url})
+                    """file_url = request.build_absolute_uri(
+                        os.path.join(settings.MEDIA_URL, f'download/{output_zip_filename}')) # стандартная ссылка в файловой системе сервера"""
+                    # print(perf_counter() - start_time) # оценка времени выполнения
+                    bucket.upload_file('backet-test', f'zipfiles/{output_zip_filename}',
+                                       os.path.join(settings.MEDIA_ROOT,
+                                                    f"download/{output_zip_filename}"))  # отправка файла в S3
+                    os.remove(
+                        os.path.join(settings.MEDIA_ROOT, f"download/{output_zip_filename}"))  # удаляем файл с сервера
+                    file_url = bucket.share_file_from_bucket('backet-test',
+                                                             f'zipfiles/{output_zip_filename}')  # ссылка на S3-хранилище
+                    return Response({'status_code': status.HTTP_200_OK, 'file_url': file_url, 'file_name': output_zip_filename})
             # если изображение вызываем метод image_process
-            elif file.name.lower().endswith(('.png', '.webp', '.jpg', '.jpeg', '.eps', '.svg')):
+            elif file.name.lower().endswith(('.png', '.webp', '.jpg', '.jpeg', '.eps', '.svg', 'ai')):
                 path = os.path.join(settings.MEDIA_ROOT, 'download')
                 new_filename = f"{datetime_name_mark}{self.image_process(file, serializer.validated_data, path, 'single_image', datetime_name_mark)}"  # вызов функции обработки изображения
-                file_url = request.build_absolute_uri(os.path.join(settings.MEDIA_URL, f'download/{new_filename}'))
-                # print(perf_counter() - start_time)
-                return Response({'status_code': status.HTTP_200_OK, 'file_url': file_url})
+                """file_url = request.build_absolute_uri(os.path.join(settings.MEDIA_URL,
+                                                                   f'download/{new_filename}'))  # стандартная ссылка в файловой системе сервера"""
+                bucket.upload_file('backet-test', f'images/{new_filename}',
+                                   os.path.join(settings.MEDIA_ROOT, f'download/{new_filename}'))  # отправка файла в S3
+                os.remove(os.path.join(settings.MEDIA_ROOT, f'download/{new_filename}'))  # удаление файла с сервера
+                file_url = bucket.share_file_from_bucket('backet-test', f'images/{new_filename}')
+                # print(perf_counter() - start_time) # оценка времени выполнения
+                return Response({'status_code': status.HTTP_200_OK, 'file_url': file_url, 'file_name': new_filename})
                 # return FileResponse(output, filename=f'processed_{file.name}', content_type='image/WEBP')
         return Response({'status_code': status.HTTP_400_BAD_REQUEST, 'message': 'Неверные параметры запроса'})
 
@@ -69,20 +84,32 @@ class ImageProcessing(APIView):
         :param tag: тег для формирования имени файла, если zip то без временной метки, иначе с ней
         :return: имя обработанного изображения
         """
-        # если получен файл зип, осталвляем только имя файла
-        if '/' in image_file.name:
-            file_name = image_file.name.split('/')[-1]
-        else:
-            file_name = image_file.name
-        # формирование имени файла
+
+        file_name = image_file.name
+        # формирование имени файла c новым форматом
         # если меняем формат
         if inp_settings['format']:
-            # сохраняём в качестве quality и формате format
-            new_file_name = file_name.split('.')[0] + f'.{inp_settings["format"]}'
+            # если пришла одиночная картинка
+            if tag == 'single_file':
+                # имя файла в формате "format"
+                new_file_name = file_name.split('.')[0] + f'.{inp_settings["format"]}'
+            # если zip-файл
+            else:
+                if image_file.name.endswith('.svg'):
+                    # проверяем, требуется ли преобразовать svg в растровый тип
+                    if not inp_settings['vector']:
+                        new_file_name = image_file.name[:image_file.name.find('.svg')] + '.' + inp_settings['format']
+                    else:
+                        new_file_name = image_file.name
+                elif image_file.name.endswith('.eps'):
+                    new_file_name = image_file.name
+                # если растровый формат
+                else:
+                    new_file_name = file_name[:file_name.find(file_name.split('.')[-1])] + inp_settings['format']
         # если формат оставить исходным
         else:
             new_file_name = file_name
-
+        # преобразование векторного изображения в растровое
         if image_file.name.lower().endswith('.svg'):
             # если параметр vector False конвертируем в формат для обработки библиотекой pillow
             if not inp_settings['vector']:
@@ -92,20 +119,19 @@ class ImageProcessing(APIView):
             # иначе сохраняем без изменений
             else:
                 if tag == 'zip':
-                    cairosvg.svg2svg(file_obj=image_file, write_to=os.path.join(path, file_name))
+                    os.makedirs(os.path.join(path, '/'.join(image_file.name.split('/')[:-1])), exist_ok=True)
+                    cairosvg.svg2svg(file_obj=image_file, write_to=os.path.join(path, new_file_name))
                 else:
                     cairosvg.svg2svg(file_obj=image_file, write_to=os.path.join(path, datetime_name_mark + file_name))
                 return image_file.name
         elif image_file.name.lower().endswith('.eps'):
-            if not inp_settings['vector']:
-                image = Image.open(image_file)
+            if tag == 'zip':
+                os.makedirs(os.path.join(path, '/'.join(image_file.name.split('/')[:-1])), exist_ok=True)
+                default_storage.save(os.path.join(path, image_file.name), image_file)
             else:
-                if tag == 'zip':
-                    default_storage.save(os.path.join(path, image_file.name), image_file)
-                else:
-                    # сохраняём файл из локального хранилища без изменений
-                    default_storage.save(os.path.join(path, datetime_name_mark + image_file.name), image_file)
-                return image_file.name
+                # сохраняём файл из локального хранилища без изменений
+                default_storage.save(os.path.join(path, datetime_name_mark + image_file.name), image_file)
+            return image_file.name
         else:
             image = Image.open(image_file)
 
@@ -129,6 +155,7 @@ class ImageProcessing(APIView):
             else:
                 image = image.resize((inp_settings['width'], inp_settings['height']))
         if tag == 'zip':
+            os.makedirs(os.path.join(path, '/'.join(new_file_name.split('/')[:-1])), exist_ok=True)
             image.save(os.path.join(path, new_file_name), quality=inp_settings['quality'],
                        format=inp_settings['format'].upper())
         elif tag == 'single_image':
@@ -153,16 +180,57 @@ class ImageProcessing(APIView):
                 path, 'w') as output_zipfile:
             # проверяем есть ли изображения в файле
             if [file.filename for file in zip_file.infolist() if
-                file.filename.lower().endswith(('.png', '.webp', '.jpg', '.jpeg', '.svg'))]:
-                os.mkdir(os.path.join(settings.MEDIA_ROOT,
-                                      f'download\\output_zip_images\\{datetime_name_mark}'))  # создаём директорию для извлечения изображений
+                file.filename.lower().endswith(('.png', '.webp', '.jpg', '.jpeg', '.svg', '.eps'))]:
                 dir_path_to_save = os.path.join(settings.MEDIA_ROOT,
-                                                f'download\\output_zip_images\\{datetime_name_mark}')  # запоминаем путь по которому следует сохранять изображения
+                                                f'download/output_zip_images/{datetime_name_mark}')  # запоминаем путь по которому следует сохранять изображения
+                os.mkdir(dir_path_to_save)  # создаём директорию для извлечения изображений
+                # извлекаем, обрабатываем, сохраняем изображения из zip
                 for file_in_zip in zip_file.infolist():
-                    if file_in_zip.filename.lower().endswith(('.png', '.webp', '.jpg', '.jpeg', '.svg')):
+                    if file_in_zip.filename.lower().endswith(('.png', '.webp', '.jpg', '.jpeg', '.svg', '.eps')):
                         with zip_file.open(file_in_zip.filename) as image_file:
                             self.image_process(image_file, inp_settings, dir_path_to_save, 'zip')
-                for output_image in os.scandir(dir_path_to_save):
-                    output_zipfile.write(output_image.path, output_image.name)
+                for output_file in os.scandir(dir_path_to_save):
+                    for img in self.extract_image_file(output_file):
+                        # print(img.find(datetime_name_mark), '/'.join(img[img.find(datetime_name_mark):].split('\\')[1:]))
+                        output_zipfile.write(img, '/'.join(img[img.find(datetime_name_mark):].split('\\')[1:]))
+                    # output_zipfile.write(output_image.path, f"{output_image.name}")
+                self.clear_and_del_dir(dir_path_to_save)
                 return True
         return False
+
+    def extract_image_file(self, file):
+        """
+        функция получает пути всех изображений из папки
+        :param file: папка или файл, пути/путь которого необходимо извлечь
+        :return: result - список путей изображений
+        """
+        result = []
+        if file.is_dir():
+            for i in os.scandir(file):
+                if i.is_file():
+                    result.append(i.path)
+                else:
+                    result.extend(self.extract_image_file(i))
+        elif file.is_file():
+            result.append(file.path)
+        return result
+
+    def clear_and_del_dir(self, path):
+        """
+        функция для удаления всех вложенных файлов и папок указанной директории
+        :param path: путь к директории
+        :return: None
+        """
+        if os.path.isdir(path):
+            files = os.scandir(path)
+            if files:
+                for file in files:
+                    if file.is_dir():
+                        self.clear_and_del_dir(file.path)
+                    else:
+                        os.remove(file.path)
+            else:
+                os.rmdir(path)
+        else:
+            os.remove(path)
+        os.rmdir(path)
