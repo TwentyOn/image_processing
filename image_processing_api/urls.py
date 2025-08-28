0a62a0ffce09bbabc176c81fef017bcba8a3ef08
Aleksey <aleksey-000@mail.ru>
2025-08-28 10:11:01 +0300

    правки путей для приложения image_processing_api

--- image_processing_api/urls.py
+++ image_processing_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ImageProcessing, GetImage
 
 urlpatterns = [
-    path('image_processing/', ImageProcessing.as_view()),
-    path('image_processing/download/<str:filename>/', GetImage.as_view()) # url на скачивание файлов
+    path('', ImageProcessing.as_view()),
+    path('download/<str:filename>/', GetImage.as_view()) # url на скачивание файлов
 ]
